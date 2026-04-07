c82d82f6c8e6f2fecee93c2a665ee9958f571e36
rene.vanderhorst <rene.vanderhorst@teamrockstars.com>
2026-04-07 15:15:06 +0200

    some fixes, still sucks

--- src/app/loan-api/loan-api.service.ts
+++ src/app/loan-api/loan-api.service.ts
@@ -11,58 +11,18 @@ export class LoanApiService {
 
     private readonly baseUrl = 'http://localhost:8080/loans';
 
-    private loans: Loan[] = [];
-
     constructor(private http: HttpClient) {
-    }
-
-    refreshLoans(): Loan[] {
-        this.http.get<Loan[]>(this.baseUrl).subscribe({
-            next: value => {
-                this.loans.length = 0;
-                value.forEach(loan => this.loans.push(loan))
-            },
-            error: err => console.error(err),
-            complete: () => console.log('Done')
-        });
-
-        console.log("Retrieved loans: ", this.loans.length)
-
-        return this.loans;
-    }
-
-    getLoanById(id: number) {
-        return this.http.get<Loan>(`${this.baseUrl}/${id}`).pipe()
     }
 
     createLoan(dto: CreateLoanDto) {
         this.http.post<Loan>(this.baseUrl, dto)
             .subscribe({
-                next: loan => { console.log("Creating new loan with id: " + loan.id) },
+                next: loan => {
+                    console.log("Creating new loan with id: " + loan.id)
+                },
                 error: err => console.error(err),
                 complete: () => console.log('New loan created')
-            }).unsubscribe();
-    }
-
-    calculate(loan: Loan) {
-        const url = `${this.baseUrl}/${loan.id}/payment-schedule/calculate`;
-
-        this.http.post<Loan>(url, loan)
-            .subscribe({
-                next: loan => console.log("Calculating payments for loan with id: " + loan.id),
-                error: err => console.error(err),
-                complete: () => console.log('Payments calculated')
-            }).unsubscribe();
-    }
-
-    deleteLoan(id: number) {
-        this.http.delete<Boolean>(`${this.baseUrl}/${id}`)
-            .subscribe({
-                next: deleted => console.log("Deleting new loan with id: " + id),
-                error: err => console.error(err),
-                complete: () => console.log('Loan deleted')
-            }).unsubscribe();
-        return id;
+            });
     }
 
 }
--- src/app/loan-overview/loan-overview.component.ts
+++ src/app/loan-overview/loan-overview.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Loan } from 'src/app/model/loan';
 import { LoanApiService } from "../loan-api/loan-api.service";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
     selector: 'loan-overview',
@@ -10,23 +11,51 @@ import { LoanApiService } from "../loan-api/loan-api.service";
 })
 export class LoanOverview {
 
-    loans: Loan[] = [];
+    private readonly baseUrl = 'http://localhost:8080/loans';
 
-    constructor(private apiService: LoanApiService) {
+    protected loans: Loan[] = [];
+
+    constructor(private http: HttpClient) {
         this.getLoans();
     }
 
     public getLoans() {
-        this.loans = this.apiService.refreshLoans();
+        this.http.get<Loan[]>(this.baseUrl).subscribe({
+            next: value => {
+                console.log("Retrieving loans: ", value.length)
+                this.loans.length = 0;
+                value.forEach(loan => this.loans.push(loan))
+            },
+            error: err => console.error(err),
+            complete: () => {
+                console.log("Retrieved loans: ", this.loans.length)
+            }
+        });
     }
 
     protected calculate(loan: Loan) {
-        this.apiService.calculate(loan);
+        const url = `${this.baseUrl}/${loan.id}/payment-schedule/calculate`;
+
+        this.http.post<Loan>(url, loan)
+            .subscribe({
+                next: loan => console.log("Calculating payments for loan with id: " + loan.id),
+                error: err => console.error(err),
+                complete: () => console.log('Payments calculated')
+            })
+
         this.getLoans();
     }
 
     protected delete(loan: Loan) {
-        this.apiService.deleteLoan(loan.id)
+        const url = `${this.baseUrl}/${loan.id}`;
+
+        this.http.delete<Boolean>(url)
+            .subscribe({
+                next: () => console.log("Deleting new loan with id: " + loan.id),
+                error: err => console.error(err),
+                complete: () => console.log('Loan deleted')
+            })
+
         this.getLoans();
     }
 }
